0a4a9befda7c809ded2d3e35d641a9ea6eb9e2cc
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-07-21 22:24:14 +0000

    connected store to employee

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -1,10 +1,9 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
 require_relative './exercise_4'
 
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+@average_revenue = @total_revenue / Store.count
+puts @total_revenue
+puts @average_revenue
