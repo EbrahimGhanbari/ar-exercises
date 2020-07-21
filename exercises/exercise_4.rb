8a3888a3eebdff83a6c940e14a5e3074a0a46aa3
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-07-21 21:58:00 +0000

    exercise 4 completed

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,5 +7,10 @@ Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens
 Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: false, womens_apparel: false)
 Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: false)
 
-puts Store.count
+
+@mens_stores = Store.all.where(mens_apparel: true)
+
+@mens_stores.each{ |item|
+  puts "#{item.name} #{item.annual_revenue}"
+}
 
