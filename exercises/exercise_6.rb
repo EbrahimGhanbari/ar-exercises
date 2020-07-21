f35cca344cb8d7452aced748818b51cff6bd7418
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-07-21 23:54:35 +0000

    validation is added

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -1,4 +1,3 @@
-
 require_relative './exercise_5'
 
 puts "Exercise 6"
@@ -6,5 +5,5 @@ puts "----------"
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "Ebrahim", last_name: "Virani", hourly_rate: 60)
-@store2.employees.create(first_name: "Mohsen", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Mohsen", last_name: "Virani", hourly_rate: 20)
 @store2.employees.create(first_name: "Khurram", last_name: "Ghanbari", hourly_rate: 60)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -1,12 +1,11 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-require_relative './exercise_4'
-require_relative './exercise_5'
 require_relative './exercise_6'
 
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+
+store_name = gets.chomp
+
+
+Store.create(name: store_name)
+puts Store.create(name: store_name).errors.messages
